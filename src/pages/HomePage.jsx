6f17157fb8412a22bc0f92675d98320001700e7d
Vishal Singh <TechnicalVishalJi@users.noreply.github.com>
2025-06-28 12:51:37 +0530

    Made a small change to deploy to vercel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+
 import { useState, useEffect, useRef } from "react"
 import { useTranslation } from "react-i18next"
 import { MessageCircle, ChevronUp, Flag, CheckCircle, Clock } from "lucide-react"
