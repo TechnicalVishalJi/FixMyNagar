fe20b62b4fcf8fbf9a72d7cf4db61aa18f52ae83
Vishal Singh <73434540+TechnicalVishalJi@users.noreply.github.com>
2025-08-06 16:44:58 +0530

    Update AdminPage.jsx

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -175,8 +175,8 @@ export default function AdminPage() {
 
   const customIcon = new L.Icon({
     iconUrl: '/gps.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
+    iconSize: [35, 41],
+    iconAnchor: [17.5, 41],
     popupAnchor: [1, -34],
     shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
     shadowSize: [41, 41],
